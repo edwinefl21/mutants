1049188cc741e5fe2c66869b5b47332bf1c9c75c
edwinefl <edwinefl21@gmail.com>
2022-06-23 22:22:04 -0500

    Update key store repository mongo

--- src/main/java/com/meli/mutant/model/DNA.java
+++ src/main/java/com/meli/mutant/model/DNA.java
@@ -11,7 +11,7 @@ import org.springframework.data.mongodb.core.mapping.Document;
 @Document(collection = "dna")
 public class DNA {
     @Id
-    private Long id;
+    private String id;
     private DNASequence dna;
     @Indexed(name = "is_mutant")
     private boolean isMutant;
--- src/main/java/com/meli/mutant/repository/IDNARepository.java
+++ src/main/java/com/meli/mutant/repository/IDNARepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface IDNARepository extends MongoRepository<DNA,Long> {
+public interface IDNARepository extends MongoRepository<DNA,String> {
 }
